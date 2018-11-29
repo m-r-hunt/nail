457db4a27de23c4a118dae084afa65118736438a
Max Hunt <max@huntw3.com>
2018-11-29 21:55:02 +0000

    Added some basic timings.

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ mod value;
 pub mod vm;
 
 use std::io::Write;
+use std::time::Instant;
 
 pub fn repl() {
     let mut vm = vm::VM::new();
@@ -39,8 +40,10 @@ pub fn repl() {
 }
 
 pub fn run_file(filename: &str) {
+    let start = Instant::now();
     let result = std::fs::read_to_string(filename);
     let code = result.expect(&format!("Unable to read file {}", filename));
+    let read_file_done = Instant::now();
 
     let mut vm = vm::VM::new();
     let result = vm.interpret(&code);
@@ -51,4 +54,6 @@ pub fn run_file(filename: &str) {
             return;
         }
     }
+    let finished = Instant::now();
+    println!("Done. File read: {}s {}ms, Interpreted: {}s {}ms.", read_file_done.duration_since(start).as_secs(), read_file_done.duration_since(start).subsec_millis(), finished.duration_since(read_file_done).as_secs(), finished.duration_since(read_file_done).subsec_millis());
 }
